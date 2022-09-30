08ff025a2a40ab9078b8301a0202e88e62821c3a
Yash Jagtap <jagtapyash2512@gmail.com>
2022-09-30 12:53:14 +0530

    Update HelloWorld.sol
    updated contract

--- contracts/HelloWorld.sol
+++ contracts/HelloWorld.sol
@@ -1,25 +1,24 @@
-//SPDX-License-Identifier: UNLICENSED
-
-pragma solidity >= 0.7.3;
-
+// SPDX-License-Identifier: UNLICENSED
+ 
+pragma solidity ^0.8.4;
+ 
 contract HelloWorld {
     //events
-    //states - variables, tokens, nfts
-    //functions - we use functions to change the state of variables
-
+    //states
+    //functions
+ 
     event messagechanged(string oldmsg, string newmsg);
-
+ 
     string public message;
-
+ 
     constructor(string memory firstmessage) {
         message = firstmessage;   
     }
-
+ 
     function update(string memory newmesssage) public {
         string memory oldmsg = message;
         message = newmesssage;
-
+ 
         emit messagechanged(oldmsg, newmesssage);
-
     }
 }
